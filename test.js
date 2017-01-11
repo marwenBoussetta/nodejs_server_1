a18397e542d84f10d0b775f3f097b562ec8f7944
marwenBoussetta <marwen.boussetta@gmail.com>
2017-01-11 21:36:11 +0100

    intialisation du serveur

---
+++ test.js
@@ -0,0 +1,34 @@
+var http = require('http');
+var url = require('url');
+
+var server = http.createServer(function(req, res) {
+    var page = url.parse(req.url).pathname;
+    console.log(page);
+    if (page == '/') {
+        res.writeHead(200, {"Content-Type": "text/plain"});
+        res.write('Vous êtes à l\'accueil, que puis-je pour vous ?');
+    }
+    else if (page == '/1') {
+    	res.writeHead(200, {"Content-Type": "text/plain"});
+		res.write('Vous êtes dans la cave à vins, ces bouteilles sont à moi !');
+    }
+    else if (page == '/2') {
+   	    res.writeHead(200, {"Content-Type": "text/html"});
+    	res.write('<!DOCTYPE html>'+
+'<html>'+
+'    <head>'+
+'        <meta charset="utf-8" />'+
+'        <title>Ma page Node.js !</title>'+
+'    </head>'+ 
+'    <body>'+
+'     	<p>Voici un paragraphe <strong>HTML</strong> !</p>'+
+'    </body>'+
+'</html>');
+    }
+    else {
+        res.writeHead(404, {"Content-Type": "text/html"});
+        res.write('NOTHING IS FOUND HERE');    
+    }
+    res.end();
+});
+server.listen(8080);
