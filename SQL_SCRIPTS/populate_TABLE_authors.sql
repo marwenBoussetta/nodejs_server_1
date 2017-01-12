87a1dcb63c99065c245642cb392c8ab8950405c3
marwenBoussetta <marwen.boussetta@gmail.com>
2017-01-12 07:30:28 +0100

    ajout des scripts sql creation base, tables et population de BDD

---
+++ SQL_SCRIPTS/populate_TABLE_authors.sql
@@ -0,0 +1,4 @@
+INSERT INTO bookshop.authors (NOM, PRENOM) VALUES ('BROWN', 'DAN');
+INSERT INTO bookshop.authors (NOM, PRENOM) VALUES ('COBEN', 'HARLEN');
+INSERT INTO bookshop.authors (NOM, PRENOM) VALUES ('VALERY', 'PAUL');
+INSERT INTO bookshop.authors (NOM, PRENOM) VALUES ('TOLSTOI', 'LEO');
